1e33f1e9ef1f74b899a4065508c6a393c603a3cf
cuongdm3 <cuongpigerr@gmail.com>
2023-03-31 09:35:50 +0700

    [update] - increase the RAM and CPU cores in config multipass

--- tools/multipass.sh
+++ tools/multipass.sh
@@ -3,7 +3,7 @@
 # Prepare the multipass VM and login to its shell.
 # This function is used at host machine.
 function run_multipass() {
-  multipass launch -n openstack -c 4 -m 6G -d 20G --mount ./:/devstack 20.04 && \
+  multipass launch -n openstack -c 6 -m 8G -d 20G --mount ./:/devstack 20.04 && \
   multipass shell openstack
 }
 
