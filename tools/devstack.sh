cef313fac374f4a4086776470d323c2c493feffb
cuongdm3 <cuongpigerr@gmail.com>
2023-03-31 11:33:57 +0700

    [stable] - Running Heat component successfully

--- tools/devstack.sh
+++ tools/devstack.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 # Install and run the OpenStack cloud.
-function stack_all() {
+stack_all() {
   cd /devstack && clear && ./stack.sh
 }
 
 # Unstack and clean the OpenStack installation.
-function unstack_all() {
+unstack_all() {
   cd /devstack && clear && ./unstack.sh && ./clean.sh
 }
 
 # Create the `stack` user.
-function create_stack_user() {
+create_stack_user() {
   sudo /devstack/tools/create-stack-user.sh
 }
 
 # Switch to `stack` user.
-function switch_to_stack_user() {
+switch_to_stack_user() {
   sudo su - stack
 }
