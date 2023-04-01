06db7992bf8259db133a4d74d5097eea460dc28f
cuongdm3 <cuongpigerr@gmail.com>
2023-04-01 23:35:35 +0700

    [dev] - prepare to create the magnum component

--- tools/devstack.sh
+++ tools/devstack.sh
@@ -2,20 +2,28 @@
 
 # Install and run the OpenStack cloud.
 stack_all() {
-  cd /devstack && clear && ./stack.sh
+    cd /devstack && clear && ./stack.sh
 }
 
 # Unstack and clean the OpenStack installation.
 unstack_all() {
-  cd /devstack && clear && ./unstack.sh && ./clean.sh
+    cd /devstack && clear && ./unstack.sh && ./clean.sh
 }
 
 # Create the `stack` user.
 create_stack_user() {
-  sudo /devstack/tools/create-stack-user.sh
+    sudo /devstack/tools/create-stack-user.sh
+}
+
+# Create then switch to the `stack` user.
+crwitch_stack_user() {
+    sudo useradd -s /bin/bash -d /opt/stack -m stack && \
+    sudo chmod +x /opt/stack && \
+    echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack && \
+    sudo -u stack -i
 }
 
 # Switch to `stack` user.
 switch_to_stack_user() {
-  sudo su - stack
+    sudo su - stack
 }
